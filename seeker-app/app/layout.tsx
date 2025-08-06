d39eedbf7440a8a06453c5241252ea20812e7cc7
Sidney-130 <janey19@DESKTOP-F726TKR.localdomain>
2025-08-06 14:21:32 +0100

    created a branch

---
+++ seeker-app/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Tektur } from "next/font/google";
+import "./globals.css";
+
+const tektur = Tektur({
+  variable: "--font-tektur",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  display: "swap",
+  preload: true,
+});
+
+export const metadata: Metadata = {
+  title: "Seeker",
+  description: "A decentralized search engine, built for the open internet",
+  icons: {
+    icon: "/images/favicon-32x32.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className={tektur.variable}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={`${tektur.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}

28a99f2d43f68098bdfa51a7b17f3a91692232b2
Sidney-130 <janey19@DESKTOP-F726TKR.localdomain>
2025-08-20 01:59:59 +0100

    result page almost done

--- src/frontend/seeker_frontend/src/components/search/searchResult.tsx
+++ src/frontend/seeker_frontend/src/components/search/searchResult.tsx
@@ -30,7 +30,6 @@ export default function SearchResults() {
 
   return (
     <div className="p-6 min-h-screen">
-      {/* Navbar */}
       <h2 className="text-2xl font-semibold mb-4">
         Search Results for: <span className="text-blue-600">{query}</span>
       </h2>
